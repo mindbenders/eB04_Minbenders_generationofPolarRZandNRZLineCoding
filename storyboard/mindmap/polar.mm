9ab33b5ca5b6934681b67103372a877d82a8bf8e
mindbenders <70203164+mindbenders@users.noreply.github.com>
2020-09-14 18:46:35 +0530

    Add files via upload

---
+++ storyboard/mindmap/polar.mm
@@ -0,0 +1,515 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#99ff66" CREATED="1600068436531" ID="ID_10276735" MODIFIED="1600089001391" TEXT="POLAR RZ AND NRZ LINE CODING">
+<node BACKGROUND_COLOR="#cccc00" CREATED="1600069099956" HGAP="57" ID="ID_249956577" MODIFIED="1600074599885" POSITION="right" VSHIFT="-48">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#666600"><b>COGNITIVE LEVEL</b></font>
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#9999ff" STYLE="sharp_bezier" WIDTH="2"/>
+<node CREATED="1600071377807" ID="ID_1776645091" MODIFIED="1600073818828" VSHIFT="-23">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#003366"><b>UNDERSTAND</b></font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1600071379672" ID="ID_967521861" MODIFIED="1600073841935">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#003366"><b>APPLY</b></font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1600071381993" HGAP="23" ID="ID_1577650979" MODIFIED="1600073895877" VSHIFT="26">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#003399"><b>ANALYZE</b></font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ffcc00" CREATED="1600069403163" HGAP="58" ID="ID_969439560" MODIFIED="1600074457984" POSITION="left" TEXT="GENERAL INFORMATION" VSHIFT="-42">
+<edge COLOR="#ff33ff" STYLE="sharp_bezier" WIDTH="2"/>
+<node CREATED="1600069624998" HGAP="22" ID="ID_124455992" MODIFIED="1600072332405" VSHIFT="-40">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#3300cc"><b>DISCIPLINE</b></font>
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1600069723718" ID="ID_143699676" MODIFIED="1600072270179" VSHIFT="-16">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#990099"><b>ELECTRONICS AND COMMUNICATION</b></font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1600069733025" ID="ID_835284479" MODIFIED="1600072249860" VSHIFT="20">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#990099"><b>ELECTRONICS AND TELECOMMUNICATION</b></font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1600069633544" HGAP="21" ID="ID_913551771" MODIFIED="1600072335192" VSHIFT="17">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000cc"><b>COURSE</b></font>
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1600069846870" HGAP="42" ID="ID_804136386" MODIFIED="1600072319051" VSHIFT="-8">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#990099"><b>COMMUNICATIPON LAB</b></font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ff33ff" CREATED="1600069488437" HGAP="98" ID="ID_1057864896" MODIFIED="1600074500643" POSITION="left" TEXT="INTRODUCTION" VSHIFT="-10">
+<edge COLOR="#66ffff" STYLE="sharp_bezier" WIDTH="2"/>
+<node CREATED="1600069913701" HGAP="28" ID="ID_1127102305" MODIFIED="1600089065211" VSHIFT="-22">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font color="#006666">TO STUDY THE GENERATION OF POLAR RZ AND NRZ LINE CODES</font></b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ff9999" CREATED="1600069973702" HGAP="69" ID="ID_1172160305" MODIFIED="1600074570262" POSITION="left" VSHIFT="32">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#993300"><b>SIMULATION</b></font>
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#ff9999" STYLE="sharp_bezier" WIDTH="2"/>
+<node CREATED="1600070103684" HGAP="62" ID="ID_1838345383" MODIFIED="1600074531112" VSHIFT="-40">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#009900"><b>VISUALIZATION</b></font>
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#66ff66" STYLE="sharp_bezier" WIDTH="2"/>
+<node CREATED="1600070217883" HGAP="21" ID="ID_160905622" MODIFIED="1600072586957" VSHIFT="-24">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#660066"><b>INTRODUCTION</b></font>
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1600070313730" HGAP="21" ID="ID_338286578" MODIFIED="1600089076669" VSHIFT="-34">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font color="#cc0000">GRASPING THE KNOWLWDGE OF </font></b>
+    </p>
+    <p>
+      <b><font color="#cc0000">POLAR RZ AND NRZ LINE CODES</font></b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1600070224199" ID="ID_1540598871" MODIFIED="1600072616889">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#660066"><b>ENTER VALUES</b></font>
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1600070657726" HGAP="22" ID="ID_1775062008" MODIFIED="1600072499433" VSHIFT="-15">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#cc0033"><b>LENGTH OF THE SEQUENCE</b></font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1600070667792" ID="ID_1219023785" MODIFIED="1600072536721" VSHIFT="14">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#cc0033"><b>BITS OF THE SEQUENCE</b></font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1600070251026" HGAP="18" ID="ID_1170868579" MODIFIED="1600072645113" VSHIFT="64">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#660066"><b>DISPLAY</b></font>
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1600070727255" HGAP="21" ID="ID_81253892" MODIFIED="1600089091620" VSHIFT="-14">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font color="#cc0033">POLAR RZ LINE CODES</font></b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1600070728798" ID="ID_1795880669" MODIFIED="1600089100526" VSHIFT="16">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font color="#cc0033">POLAR NRZ LINE CODES</font></b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1600070145891" HGAP="62" ID="ID_254453616" MODIFIED="1600073280192" VSHIFT="-22">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#006666"><b>PATHWAYS</b></font>
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1600070828158" ID="ID_1229495472" MODIFIED="1600073383511">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#000099"><b>CLICK ON SIMULATOR</b></font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1600070829909" ID="ID_1636535400" MODIFIED="1600073405782">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#000099"><b>CLICK ON INTRODUCTION</b></font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1600070832341" ID="ID_1604164463" MODIFIED="1600073435069">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#000099"><b>UNDERSTAND THE EXPERIMENT</b></font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1600070833973" ID="ID_422715573" MODIFIED="1600073455777">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#000099"><b>CLICK ON EXPERIMENT</b></font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1600070835485" ID="ID_1394592822" MODIFIED="1600073478976">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#000099"><b>ENTER THE LENGTH OF BIT SEQUENCE</b></font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1600070837173" ID="ID_983075290" MODIFIED="1600073505144">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#000099"><b>ENTER THE BIT SEQUENCE</b></font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1600070838645" ID="ID_356291029" MODIFIED="1600073526216">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#000099"><b>CHOOSE RZ/NRZ/BOTH</b></font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1600070840237" ID="ID_1362842558" MODIFIED="1600073553720">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#330099"><b>CLICK SUBMIT</b></font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1600071037581" ID="ID_1957138447" MODIFIED="1600073573098">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#330099"><b>VIEW RESULTS</b></font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#99ffcc" CREATED="1600071449645" HGAP="67" ID="ID_147488695" MODIFIED="1600074638842" POSITION="right" VSHIFT="-129">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#006666"><b>ASSESMENT</b></font>
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#00cccc" STYLE="sharp_bezier" WIDTH="2"/>
+<node CREATED="1600071498416" HGAP="27" ID="ID_978202429" MODIFIED="1600073723038" VSHIFT="-30">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#cc0000"><b>FORMATIVE</b></font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1600071501021" HGAP="33" ID="ID_662251356" MODIFIED="1600073745983" VSHIFT="33">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#990000"><b>SUMMATIVE</b></font>
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1600071544832" HGAP="15" ID="ID_1535713980" MODIFIED="1600073799083" VSHIFT="-19">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#003366"><b>PRE-TEST</b></font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1600071555159" HGAP="17" ID="ID_314412441" MODIFIED="1600073782416" VSHIFT="17">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#003366"><b>POST-TEST</b></font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ccccff" CREATED="1600071646125" HGAP="37" ID="ID_417083220" MODIFIED="1600074679705" POSITION="right" VSHIFT="-28">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#000066"><b>FOCUS AREA</b></font>
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#ffff66" STYLE="sharp_bezier" WIDTH="2"/>
+<node CREATED="1600071657580" ID="ID_1766775575" MODIFIED="1600074238756" VSHIFT="-22">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#ff00cc"><b>REINFORCE THEORITICAL CONCEPT</b></font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1600071664140" HGAP="23" ID="ID_419949952" MODIFIED="1600089124867" VSHIFT="22">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font color="#ff00ff">CONVERT BINARY SEQUENCE INTO </font></b>
+    </p>
+    <p>
+      <b><font color="#ff00ff">POLAR RZ AND NRZ LINE CODES</font></b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node BACKGROUND_COLOR="#99ffff" CREATED="1600071878980" HGAP="38" ID="ID_1463537210" MODIFIED="1600089164904" POSITION="right" TEXT="KNOWLEDGE" VSHIFT="58">
+<edge COLOR="#9999ff" STYLE="sharp_bezier" WIDTH="2"/>
+<arrowlink DESTINATION="ID_1463537210" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1077694326" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1463537210" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1077694326" SOURCE="ID_1463537210" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1600071899378" HGAP="16" ID="ID_393923521" MODIFIED="1600089166353" VSHIFT="-17">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font color="#ff0000">POLAR RZ LINE CODES</font></b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1600071929072" ID="ID_373164167" MODIFIED="1600089152221" VSHIFT="15">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font color="#ff3333">POLAR NRZ LINE CODES</font></b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</map>
